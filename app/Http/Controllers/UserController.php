858555468313af2b1954c9ac4ac5c380c59336e9
lucas <lucas.caceres.lc@hotmail.com>
2026-02-13 20:05:02 -0300

    get-active

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -43,7 +44,7 @@ class UserController extends Controller
     public function show()
     {
         try {
-            $user = Auth::user()->load('subscription');
+            $user = Auth::user()->load('activeSubscription');
 
             $hasLocal = $user->locales()->exists();
 
